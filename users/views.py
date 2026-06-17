bbae06ab87a5dca145b6cd6990bd61cd87620ea1
Yessimkhan Azamat <yes_im_khann@mail.ru>
2026-06-17 21:30:18 +0500

    database connection added

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from .serializers import RegisterSerializer
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
