14e8164ae21e1326c18568d4b9ccd4c68f9cca47
kenshiwhite <221439@astanait.edu.kz>
2026-06-18 17:59:22 +0500

    aad

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, permissions
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, SupplierSerializer
 from django.contrib.auth import get_user_model
 
 
