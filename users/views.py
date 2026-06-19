17077bf7131f85404c70d74891a7870d3b97f450
kenshiwhite <221439@astanait.edu.kz>
2026-06-19 09:53:05 +0500

    ada

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics, permissions
 from .serializers import RegisterSerializer, SupplierSerializer
 from django.contrib.auth import get_user_model
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 User = get_user_model()
 
@@ -16,3 +17,18 @@ class SupplierListView(generics.ListAPIView):
 
     def get_queryset(self):
         return User.objects.filter(role='supplier')
+    
+
+class MeView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            'id': user.id,
+            'username': user.username,
+            'email': user.email,
+            'role': user.role,
+            'company_name': user.company_name,
+            'phone': user.phone,
+        })
