3e2e10e6000bcefb34a3f913f62fb444097b2749
kenshiwhite <221439@astanait.edu.kz>
2026-06-19 09:53:48 +0500

    ada

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import RegisterView, SupplierListView
+from .views import RegisterView, SupplierListView, MeView
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('suppliers/', SupplierListView.as_view()),  # ← add this
-
+    path('me/', MeView.as_view()),
 ]
